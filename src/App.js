e725223492ce41cbfb55bcbe973bfb04d893bb5c
Jithin S <jithins41@gmail.com>
2021-05-28 21:58:02 +0530

    After Create Counter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import React from 'react'
+import { useState } from 'react'
+import Counter from './Components/Counter'
+import Header from './Components/Header'
+
+
+
+
 function App() {
+  const [count, setCount] = useState(0)
+  const updateCounter = () => {
+    setCount(
+      count+1
+    )
+  }
+
   return (
-    <h1>Hello React JS</h1>
-  );
+    <div>
+      <button onClick={updateCounter}>+</button>
+      <Counter count={count}/>
+    </div>
+  )
 }
 
-export default App;
+export default App
+
---
+++ src/Components/Counter.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Counter(props) {
+    return (
+        <div>
+            <h3>Counter : {props.count}</h3>
+        </div>
+    )
+}
+
+export default Counter
